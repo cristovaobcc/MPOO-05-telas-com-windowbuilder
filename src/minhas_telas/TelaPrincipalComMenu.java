fcd7b4f32f1c5751c76205b059f552310626083a
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-19 16:44:23 -0300

    Add MenuBar, Menu Cadastro e MenuItem "Novo funcionario"

--- src/minhas_telas/TelaPrincipalComMenu.java
+++ src/minhas_telas/TelaPrincipalComMenu.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class TelaPrincipalComMenu extends JFrame {
 
@@ -31,8 +34,23 @@ public class TelaPrincipalComMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipalComMenu() {
+		setTitle("AppPrincipal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenuCadastro = new JMenu("Cadastro");
+		mnNewMenuCadastro.setMnemonic('C');
+		menuBar.add(mnNewMenuCadastro);
+		
+		JMenu mnNewMenu_1 = new JMenu("Funcionário");
+		mnNewMenu_1.setMnemonic('F');
+		mnNewMenuCadastro.add(mnNewMenu_1);
+		
+		JMenuItem mntmMenuItemNovoFuncionario = new JMenuItem("Novo Funcionario");
+		mnNewMenu_1.add(mntmMenuItemNovoFuncionario);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
