2335cea4f981365f9dc3892ab7654884bd632393
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-19 17:39:07 -0300

    Acrescenta elementos à janela DadosDoGerente.java

--- src/minhas_telas/DadosDoGerente.java
+++ src/minhas_telas/DadosDoGerente.java
@@ -9,10 +9,17 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class DadosDoGerente extends JFrame {
 
 	private JPanel contentPaneDadosDeGerente;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -46,19 +53,48 @@ public class DadosDoGerente extends JFrame {
 		contentPaneDadosDeGerente.add(tabbedPaneGerente, BorderLayout.CENTER);
 		
 		JPanel panelDadosGerente = new JPanel();
+		panelDadosGerente.setBorder(new TitledBorder(null, "Dados pessoais do gerente", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		tabbedPaneGerente.addTab("Dados Pessoais", null, panelDadosGerente, null);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNome.setFont(new Font("Dialog", Font.BOLD, 14));
+		
+		textField = new JTextField();
+		textField.setColumns(10);
 		GroupLayout gl_panelDadosGerente = new GroupLayout(panelDadosGerente);
 		gl_panelDadosGerente.setHorizontalGroup(
 			gl_panelDadosGerente.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 785, Short.MAX_VALUE)
+				.addGroup(gl_panelDadosGerente.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNome, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(textField, GroupLayout.DEFAULT_SIZE, 616, Short.MAX_VALUE)
+					.addGap(47))
 		);
 		gl_panelDadosGerente.setVerticalGroup(
 			gl_panelDadosGerente.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 533, Short.MAX_VALUE)
+				.addGroup(gl_panelDadosGerente.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panelDadosGerente.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNome, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(466, Short.MAX_VALUE))
 		);
 		panelDadosGerente.setLayout(gl_panelDadosGerente);
 		
 		JPanel panelLojasSupervisionadas = new JPanel();
+		panelLojasSupervisionadas.setBorder(new TitledBorder(null, "Lista de lojas supervisionadas", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		tabbedPaneGerente.addTab("Lojas supervisionadas", null, panelLojasSupervisionadas, null);
+		GroupLayout gl_panelLojasSupervisionadas = new GroupLayout(panelLojasSupervisionadas);
+		gl_panelLojasSupervisionadas.setHorizontalGroup(
+			gl_panelLojasSupervisionadas.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 785, Short.MAX_VALUE)
+		);
+		gl_panelLojasSupervisionadas.setVerticalGroup(
+			gl_panelLojasSupervisionadas.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 533, Short.MAX_VALUE)
+		);
+		panelLojasSupervisionadas.setLayout(gl_panelLojasSupervisionadas);
 	}
 }
